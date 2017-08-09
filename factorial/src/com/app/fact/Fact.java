4c3b8e09f1c838524df44a4df993fbed86aba0fd
DELL___VOSTRO <DELL___VOSTRO@192.168.2.29>
2017-08-10 03:31:27 +0530

    fcommit

---
+++ factorial/src/com/app/fact/Fact.java
@@ -0,0 +1,13 @@
+package com.app.fact;
+
+public class Fact {
+       public static void main(String[] args) {
+	 String s="my name is lakhan";
+	 String s2[]=s.split(" ");
+	 for(int i=s2.length-1;i>=0;i--){
+    	 
+		    
+	  System.out.println(s2[i]+" ");
+	}
+}
+}
