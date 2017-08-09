69c94dc9ac2334a406ead9f5adc62e07f51967a7
DELL___VOSTRO <DELL___VOSTRO@192.168.2.29>
2017-08-10 02:25:32 +0530

    initialcomit

---
+++ PrimeNO/src/com/app/PrimeCheck.java
@@ -0,0 +1,19 @@
+package com.app;
+
+public class PrimeCheck {
+public static void main(String[] args) {
+	int num=13;
+	int count=0;
+	for(int i=2;i<num;i++){
+		if(num%i==0){
+			count++;
+			break;
+		}
+	}
+	if(count==0){
+		System.out.println("This is prime number");
+	}else{
+		System.out.println("This is not prime number");
+	}
+}
+}
