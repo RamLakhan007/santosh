262a5b893614e1788937a63d303a20a9d9c24928
DELL___VOSTRO <DELL___VOSTRO@192.168.2.29>
2017-08-10 03:56:15 +0530

    hjhj

--- factorial/src/com/app/fact/Fact.java
+++ factorial/src/com/app/fact/Fact.java
@@ -5,7 +5,7 @@ public class Fact {
 	 String s="my name is lakhan";
 	 String s2[]=s.split(" ");
 	 for(int i=s2.length-1;i>=0;i--){
-    	 
+    	 System.out.println("oooooooooooooooooooooooooooooooooo99999");
 		    
 	  System.out.println(s2[i]+" ");
 	}
