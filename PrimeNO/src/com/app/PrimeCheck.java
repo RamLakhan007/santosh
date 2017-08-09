9262d0f05a5c08f6ebc090e8fed63fe6fd4ae858
RamLakhan007 <30878043+RamLakhan007@users.noreply.github.com>
2017-08-10 02:48:49 +0530

    Update PrimeCheck.java

--- PrimeNO/src/com/app/PrimeCheck.java
+++ PrimeNO/src/com/app/PrimeCheck.java
@@ -8,8 +8,9 @@ public static void main(String[] args) {
 	for(int i=2;i<num;i++){
 		if(num%i==0){
 			count++;
-			break; 
+			 
 			System.out.println("This is a mango");
+			break;
 		}
 	}
 	if(count==0){
