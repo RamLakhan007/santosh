7224ac73c8b5f05c80c890a4a0202a2451e3da78
DELL___VOSTRO <DELL___VOSTRO@192.168.2.29>
2017-08-10 03:18:27 +0530

    sec

--- PrimeNO/src/com/app/PrimeCheck.java
+++ PrimeNO/src/com/app/PrimeCheck.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	for(int i=2;i<num;i++){
 		if(num%i==0){
 			count++;
-			 
+			 System.out.println("ooooooooooooooooooooooooooooooooooo");
 			System.out.println("This is a mango");
 			break;
 		}
