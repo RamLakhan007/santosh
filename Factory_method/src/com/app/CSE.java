11627dd7fc1d8257eeaf6ecee2f039afb8f4acbd
DELL___VOSTRO <DELL___VOSTRO@192.168.2.29>
2017-08-11 00:44:44 +0530

    modify

--- Factory_method/src/com/app/CSE.java
+++ Factory_method/src/com/app/CSE.java
@@ -1,10 +1,9 @@
 package com.app;
 
 public class CSE implements Fees{
-
 	@Override
 	public double showFees() {
-		
+		System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkk");
 		return 60000.00;
 	}
 	
