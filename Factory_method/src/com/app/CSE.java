3b059023776de809af0ff0bcb0123f9047e35c23
RamLakhan007 <30878043+RamLakhan007@users.noreply.github.com>
2017-08-11 00:45:37 +0530

    Update CSE.java

--- Factory_method/src/com/app/CSE.java
+++ Factory_method/src/com/app/CSE.java
@@ -3,9 +3,9 @@ package com.app;
 public class CSE implements Fees{
 	@Override
 	public double showFees() {
-		System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkk");
-		System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkk");
-		System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkk");
+		System.out.println("kkkkkkkkk11111111111111111111kkkkkkkkkkkkkkkkkk");
+		System.out.println("kkkkkkkkkkk222222222222222222222kkkkkkkkkkkkkkkk");
+		System.out.println("kkkkkkkkkkkk333333333333333333333kkkkkkkkkkkkkkk");
 		return 60000.00;
 	}
 	
